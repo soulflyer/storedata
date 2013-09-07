b536f64223997e896acc9c503cff66dfe529b739
Iain <public@soulflyer.co.uk>
2013-09-07 22:24:54 +0700

    created .gitignore and removed non essential stuff

--- platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/iain/Code/workspace/storedata2/platforms/android/bin/AndroidManifest.xml : \
-/Users/iain/Code/workspace/storedata2/platforms/android/AndroidManifest.xml \
--- platforms/android/bin/Storedata-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/iain/Code/workspace/storedata2/platforms/android/bin/Storedata-debug-unaligned.apk : \
-/Users/iain/Code/workspace/storedata2/platforms/android/bin/Storedata.ap_ \
-/Users/iain/Code/workspace/storedata2/platforms/android/bin/classes.dex \
-/Users/iain/Code/workspace/storedata2/platforms/android/libs/cordova-3.0.0.jar \
