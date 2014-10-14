3c351674231278a16dda8a683f4238644e03ac3d
Iain <public@soulflyer.co.uk>
2014-10-14 17:41:35 +0700

    write and read to android and iOS file systems

--- www/js/common.js
+++ www/js/common.js
@@ -2,43 +2,68 @@
 // This is passed as a parameter to readLocal and must be a function not a simple type.
 // Simple types are passed by value and the result will therefore be discarded without us ever
 // seeing it.
+var savedFilesystem;
+var downloadDirectory;
+var localFileSystemName="BPData";
 
 function returnedResult(){
     this.value="default value";
 }
 
-
 window.onload = init;
 function init() {
-    // Add an event listener for deviceready
+    alert("Hello");
     document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady() {
-
-    alert("networkConnected: " + networkConnected());
-
-    localFileSystemName="BPData";
-
-    setupLocalFileSystem();
-
-    //window.requestFileSystem( LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess , null );
-     alert("Filesystem request returned: " + downloadDirectory.fullPath);
-    download();
-    alert("Download call returned. Hit OK to read contents of file from local storage");
-    readLocal("genres", returnedResult);
-    alert("ReadLocal returned: " + returnedResult.value);
-
-    mkdirLocal("testdir");
-
-    writeLocal("testdir/passwd2","Some random stuff");
-    readLocal("testdir/passwd2", returnedResult);
-    alert("readLocal returned: " + returnedResult.value);
+    // localFileSystemName="BPData";
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, null);
 }
 
-function download() {
-    var fileURL = "http://bp.wiserobot.com/flashcardsapi/api/getGenres";
+function onFileSystemSuccess(fileSystem) {
+    window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function(fentry){
+        console.log("Data directory: " + fentry.fullPath);
+        fentry.getDirectory(localFileSystemName , {create:true}, doDownloadTests , fail);
+    });
+}
+
+function doDownloadTests(dirEntry){
+    console.log("Filesystem request returned dirEntry: " + dirEntry.fullPath);
+    var fileURL = "http://barperfe.nextmp.net/flashcardsapi/api/getSubjectsfile";
     var localFileName = "genres";
-    alert('Downloading ' + fileURL + " to: " + localFileName);
-    downloadFile(fileURL,localFileName);
+    console.log('Downloading ' + fileURL + " to: " + localFileName);
+    dirEntry.getFile(localFileName, {create: true}, doDownloadFile , fail);
+    dirEntry.getFile(localFileName, null , doReadFile , fail);
+}
+
+function doReadFile(fileEntry){
+    fileEntry.file(gotFile, fail);
+    function gotFile(gfile){
+        var reader=new FileReader;
+        reader.onloadend= function(evt){
+            var txtArea = document.createElement('textarea');
+            txtArea.value = this.result;
+            document.body.appendChild(txtArea);
+            console.log("Read returned: " + this.result);
+        };
+        reader.readAsText(gfile);
+    }
+}
+
+function doDownloadFile(fileEntry){
+    console.log("Entering doDownloadFile");
+    console.log("fileEntry.fullPath: " + fileEntry.fullPath);
+    var fileURL = "http://barperfe.nextmp.net/flashcardsapi/api/getSubjectsfile";
+    var fileTransfer = new FileTransfer();
+    fileTransfer.download(fileURL,
+                          fileEntry.fullPath,
+                          null,
+                          function(error){
+                              alert("Download error source " + JSON.stringify(error));
+                          } );
+}
+
+function fail(error) {
+    alert('We encountered a problem: ' + error.code);
 }
