0d9e0e69f68d5ae22925ef58dc84d683e518ecaf
Iain <public@soulflyer.co.uk>
2014-10-16 21:03:46 +0700

    created readLocal and writeLocal functions

--- www/js/common.js
+++ www/js/common.js
@@ -1,9 +1,3 @@
-// returnedResults is somewhere to store the contents of a file read from the local file system
-// This is passed as a parameter to readLocal and must be a function not a simple type.
-// Simple types are passed by value and the result will therefore be discarded without us ever
-// seeing it.
-var savedFilesystem;
-var downloadDirectory;
 var localFileSystemName="BPData";
 
 function returnedResult(){
@@ -17,48 +11,64 @@ function init() {
 }
 
 function onDeviceReady(){
-    window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function(fentry){
-        console.log("Data directory: " + fentry.fullPath);
-        fentry.getDirectory(localFileSystemName , {create:true}, doDownloadTests , fail);
-    });
-}
-
-function doDownloadTests(dirEntry){
-    console.log("Filesystem request returned dirEntry: " + dirEntry.fullPath);
-    var fileURL = "http://barperfe.nextmp.net/flashcardsapi/api/getSubjectsfile";
-    var localFileName = "genres";
-    console.log('Downloading ' + fileURL + " to: " + localFileName);
-    dirEntry.getFile(localFileName, {create: true}, doDownloadFile , fail);
-    dirEntry.getFile(localFileName, null , doReadFile , fail);
-}
-
-function doReadFile(fileEntry){
-    fileEntry.file(gotFile, fail);
-    function gotFile(gfile){
-        var reader=new FileReader;
-        reader.onloadend= function(evt){
-            var txtArea = document.createElement('textarea');
-            txtArea.value = this.result;
-            document.body.appendChild(txtArea);
-            console.log("Read returned: " + this.result);
-        };
-        reader.readAsText(gfile);
-    }
-}
-
-function doDownloadFile(fileEntry){
-    console.log("Entering doDownloadFile");
-    console.log("fileEntry.fullPath: " + fileEntry.fullPath);
-    var fileURL = "http://barperfe.nextmp.net/flashcardsapi/api/getSubjectsfile";
-    var fileTransfer = new FileTransfer();
-    fileTransfer.download(fileURL,
-                          fileEntry.fullPath,
-                          null,
-                          function(error){
-                              alert("Download error source " + JSON.stringify(error));
-                          } );
+    // window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function(fentry){
+    //     console.log("Data directory: " + fentry.fullPath);
+    //     fentry.getDirectory(localFileSystemName , {create:true}, doDownloadTests , fail);
+    // });
+    writeLocal("genres3","http://barperfe.nextmp.net/flashcardsapi/api/getSubjectsfile");
+    readLocal("genres3");
 }
 
 function fail(error) {
     alert('We encountered a problem: ' + error.code);
+}
+
+function readLocal(fileName){
+    console.log("filename: " + fileName);
+    window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function(fentry){
+        console.log("Data directory: " + fentry.fullPath);
+        fentry.getDirectory(localFileSystemName,
+                            null,
+                            function(dirEntry){
+                                dirEntry.getFile(fileName, {create: true}, doRead , fail);},
+                            fail);
+    });
+    function doRead(fileEntry){
+        console.log("Entering doRead");
+        console.log("fileEntry.fullPath: " + fileEntry.fullPath);
+        fileEntry.file(gotFile, fail);
+        function gotFile(gfile){
+            var reader=new FileReader;
+            reader.onloadend= function(evt){
+                var txtArea = document.createElement('textarea');
+                txtArea.value = this.result;
+                document.body.appendChild(txtArea);
+                console.log("Read returned: " + this.result);
+            };
+            reader.readAsText(gfile);
+        }
+    }
+}
+
+function writeLocal(fileName,fileURL){
+    console.log("filename: " + fileName + " fileURL: " + fileURL);
+    window.resolveLocalFileSystemURL(cordova.file.applicationStorageDirectory, function(fentry){
+        console.log("Data directory: " + fentry.fullPath);
+        fentry.getDirectory(localFileSystemName,
+                            {create:true},
+                            function(dirEntry){
+                                dirEntry.getFile(fileName, {create: true}, doDownload , fail);},
+                            fail);
+    });
+    function doDownload(fileEntry){
+        console.log("Entering doDownload");
+        console.log("fileEntry.fullPath: " + fileEntry.fullPath);
+         var fileTransfer = new FileTransfer();
+        fileTransfer.download(fileURL,
+                              fileEntry.fullPath,
+                              null,
+                              function(error){
+                                  alert("Download error source " + JSON.stringify(error));
+                              });
+    }
 }
